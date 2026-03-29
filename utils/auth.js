f8decefc0255770020a8145b9db61e121c31de5d
Gaio Augusto Moraes dos Santos <gaioaugusto16@gmail.com>
2026-03-29 09:32:48 -0400

    cleaning up unused/deprecated parts of the project

--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const hashPassword = async (password) => {
-  return await bcrypt.hash(password, 10);
-};
-
-export const verifyPassword = async (password, hash) => {
-  return await bcrypt.compare(password, hash);
-};
