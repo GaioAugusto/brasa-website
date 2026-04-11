1a1ee636821bce31167f94e2b21b7b17da79f590
Gaio Augusto Moraes dos Santos <gaioaugusto16@gmail.com>
2026-04-10 21:59:07 -0400

    opportunitiesPage refactored image paths

--- src/pages/Opportunities/index.tsx
+++ src/pages/Opportunities/index.tsx
@@ -1,55 +1,55 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useLocale } from "../../contexts/Locale";
 import { soccerCaptain, volleyballCaptain } from "../../utilities/teamCaptains";
 import { OpportunitiesProps, Team } from "./types";
 import { OpportunitiesView } from "./view";
-import { useEffect } from "react";
 
 type ComponentType = React.FC<OpportunitiesProps>;
 export const Opportunities: ComponentType = () => {
-  const { commonLocale, templatesLocale } = useLocale();
-  const { state } = useLocation() as { state: { scrollTo?: string } | null };
+    const { commonLocale, templatesLocale } = useLocale();
+    const { state } = useLocation() as { state: { scrollTo?: string } | null };
 
-  const soccerTeam: Team = {
-    title: commonLocale.get("soccerTeam"),
-    description: templatesLocale.get("soccerTeamDescription"),
-    dialog: true,
-    image: "/soccer1.jpeg",
-    descriptionDialog: templatesLocale.get("dialogDescription"),
-    imageDialog: "/soccer2.jpeg",
-    captain: soccerCaptain,
-    youtubeUrl: "https://www.youtube.com/@Galatasarados",
-    instagramUrl: "https://www.instagram.com/galatasaradosfc/",
-  };
-  const volleyballTeam: Team = {
-    title: commonLocale.get("volleyballTeam"),
-    description: templatesLocale.get("volleyballTeamDescription"),
-    dialog: true,
-    image: "/brasavolei1.jpeg",
-    descriptionDialog: templatesLocale.get("dialogDescription"),
-    imageDialog: "/brasavolei2.jpeg",
-    captain: volleyballCaptain,
-  };
+    const soccerTeam: Team = {
+        title: commonLocale.get("soccerTeam"),
+        description: templatesLocale.get("soccerTeamDescription"),
+        dialog: true,
+        image: "/opportunities/students/soccer1.jpeg",
+        descriptionDialog: templatesLocale.get("dialogDescription"),
+        imageDialog: "/opportunities/students/soccer2.jpeg",
+        captain: soccerCaptain,
+        youtubeUrl: "https://www.youtube.com/@Galatasarados",
+        instagramUrl: "https://www.instagram.com/galatasaradosfc/",
+    };
+    const volleyballTeam: Team = {
+        title: commonLocale.get("volleyballTeam"),
+        description: templatesLocale.get("volleyballTeamDescription"),
+        dialog: true,
+        image: "/opportunities/students/brasavolei1.jpeg",
+        descriptionDialog: templatesLocale.get("dialogDescription"),
+        imageDialog: "/opportunities/students/brasavolei2.jpeg",
+        captain: volleyballCaptain,
+    };
 
-  useEffect(() => {
-    if (state?.scrollTo) {
-      if (state.scrollTo === "top") {
-        // Scroll to the top of the page
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      } else {
-        // Scroll to a specific section
-        const section = document.getElementById(state.scrollTo);
-        if (section) {
-          section.scrollIntoView({ behavior: "smooth" });
+    useEffect(() => {
+        if (state?.scrollTo) {
+            if (state.scrollTo === "top") {
+                // Scroll to the top of the page
+                window.scrollTo({ top: 0, behavior: "smooth" });
+            } else {
+                // Scroll to a specific section
+                const section = document.getElementById(state.scrollTo);
+                if (section) {
+                    section.scrollIntoView({ behavior: "smooth" });
+                }
+            }
         }
-      }
-    }
-  }, [state]);
+    }, [state]);
 
-  return (
-    <OpportunitiesView
-      soccerTeam={soccerTeam}
-      volleyballTeam={volleyballTeam}
-    />
-  );
+    return (
+        <OpportunitiesView
+            soccerTeam={soccerTeam}
+            volleyballTeam={volleyballTeam}
+        />
+    );
 };
--- src/utilities/teamCaptains.ts
+++ src/utilities/teamCaptains.ts
@@ -1,18 +1,18 @@
 import { TeamCaptain } from "../pages/Opportunities/types";
 
 export const soccerCaptain: TeamCaptain = {
-  name: "Henrique Pedras",
-  phone: "+1 (703) 509-0553",
-  email: "henriquepedras38@gmail.com",
-  instagram: "https://www.instagram.com/riquepedras/",
-  image: "/soccerCaptain.jpeg",
-  instagramUser: "@riquepedras",
+    name: "Henrique Pedras",
+    phone: "+1 (703) 509-0553",
+    email: "henriquepedras38@gmail.com",
+    instagram: "https://www.instagram.com/riquepedras/",
+    image: "/opportunities/students/soccerCaptain.jpeg",
+    instagramUser: "@riquepedras",
 };
 export const volleyballCaptain: TeamCaptain = {
-  name: "Paulo Maia",
-  phone: "+55 86 8802-7909",
-  email: "paulo.cortellazzi@mail.utoronto.ca", // change this to paulao email
-  instagram: "https://www.instagram.com/paulomaiacortellazzi/",
-  image: "/volleyballCaptain.jpeg",
-  instagramUser: "@paulomaiacortellazzi",
+    name: "Paulo Maia",
+    phone: "+55 86 8802-7909",
+    email: "paulo.cortellazzi@mail.utoronto.ca", // change this to paulao email
+    instagram: "https://www.instagram.com/paulomaiacortellazzi/",
+    image: "/opportunities/students/volleyballCaptain.jpeg",
+    instagramUser: "@paulomaiacortellazzi",
 };
