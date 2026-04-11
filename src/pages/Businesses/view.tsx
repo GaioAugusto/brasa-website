012c8732584b97c441bb37abeb2274e128d5f181
Gaio Augusto Moraes dos Santos <gaioaugusto16@gmail.com>
2026-04-11 11:10:21 -0400

    opportunities page and strings

--- src/pages/Businesses/view.tsx
+++ src/pages/Businesses/view.tsx
@@ -17,6 +17,40 @@ type ComponentType = React.FC<BusinessesPageViewProps>;
 
 export const BusinessesPageView: ComponentType = () => {
     const { commonLocale, templatesLocale } = useLocale();
+    const highlights = [
+        {
+            title: templatesLocale.get("partnerHighlightCommunityTitle"),
+            description: templatesLocale.get(
+                "partnerHighlightCommunityDescription",
+            ),
+        },
+        {
+            title: templatesLocale.get("partnerHighlightBrandTitle"),
+            description: templatesLocale.get(
+                "partnerHighlightBrandDescription",
+            ),
+        },
+        {
+            title: templatesLocale.get("partnerHighlightExperiencesTitle"),
+            description: templatesLocale.get(
+                "partnerHighlightExperiencesDescription",
+            ),
+        },
+    ];
+    const impactMetrics = [
+        {
+            value: "180+",
+            label: templatesLocale.get("impactHighlightTulumLabel"),
+        },
+        {
+            value: "100+",
+            label: templatesLocale.get("impactHighlightConcidLabel"),
+        },
+        {
+            value: "12+",
+            label: templatesLocale.get("impactHighlightPartnersLabel"),
+        },
+    ];
 
     return (
         <S.Container id="/businesses">
@@ -25,7 +59,82 @@ export const BusinessesPageView: ComponentType = () => {
                 subtitle={templatesLocale.get("partnerUp")}
             />
 
-            <InfiniteCarousel images={images} speed={20} />
+            <S.ContentWrapper>
+                <S.StorySection>
+                    <S.StoryGrid>
+                        <S.StoryBody>
+                            <S.SectionTitle>
+                                {templatesLocale.get("businessStoryTitle")}
+                            </S.SectionTitle>
+                            <S.Paragraph>
+                                {templatesLocale.get("businessStoryParagraph1")}
+                            </S.Paragraph>
+                            <S.Paragraph>
+                                {templatesLocale.get("businessStoryParagraph2")}
+                            </S.Paragraph>
+                            <S.Paragraph>
+                                {templatesLocale.get("businessStoryParagraph3")}
+                            </S.Paragraph>
+
+                            <S.DownloadArea>
+                                <S.DownloadButton type="button">
+                                    {templatesLocale.get(
+                                        "partnershipPackageCta",
+                                    )}
+                                </S.DownloadButton>
+                                <S.DownloadHint>
+                                    {templatesLocale.get(
+                                        "partnershipPackageHint",
+                                    )}
+                                </S.DownloadHint>
+                            </S.DownloadArea>
+                        </S.StoryBody>
+
+                        <S.ImpactPanel>
+                            <S.ImpactTitle>
+                                {templatesLocale.get("impactHighlightsTitle")}
+                            </S.ImpactTitle>
+                            <S.MetricsList>
+                                {impactMetrics.map((metric) => (
+                                    <S.MetricCard key={metric.label}>
+                                        <S.MetricValue>
+                                            {metric.value}
+                                        </S.MetricValue>
+                                        <S.MetricLabel>
+                                            {metric.label}
+                                        </S.MetricLabel>
+                                    </S.MetricCard>
+                                ))}
+                            </S.MetricsList>
+                        </S.ImpactPanel>
+                    </S.StoryGrid>
+                </S.StorySection>
+
+                <S.PartnersSection>
+                    <S.SectionTitle>
+                        {templatesLocale.get("ourPartnersTitle")}
+                    </S.SectionTitle>
+                    <InfiniteCarousel images={images} speed={20} />
+                </S.PartnersSection>
+
+                <S.HighlightsSection>
+                    <S.SectionTitle>
+                        {templatesLocale.get("whyPartnerTitle")}
+                    </S.SectionTitle>
+                    <S.HighlightsGrid>
+                        {highlights.map((highlight) => (
+                            <S.HighlightCard key={highlight.title}>
+                                <S.HighlightTitle>
+                                    {highlight.title}
+                                </S.HighlightTitle>
+                                <S.HighlightDescription>
+                                    {highlight.description}
+                                </S.HighlightDescription>
+                            </S.HighlightCard>
+                        ))}
+                    </S.HighlightsGrid>
+                </S.HighlightsSection>
+            </S.ContentWrapper>
         </S.Container>
     );
 };
