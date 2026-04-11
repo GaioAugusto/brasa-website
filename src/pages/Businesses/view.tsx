8a55dfa09d33681049f7bf9f560c3f1b61125d9b
Gaio Augusto Moraes dos Santos <gaioaugusto16@gmail.com>
2026-04-11 10:51:46 -0400

    novo carrossel de parcerias

--- src/pages/Businesses/view.tsx
+++ src/pages/Businesses/view.tsx
@@ -1,16 +1,31 @@
 import { Header } from "../../components/Header";
 import { useLocale } from "../../contexts/Locale";
+import { InfiniteCarousel } from "./components";
+import * as S from "./styles";
 import { BusinessesPageViewProps } from "./types";
 
+const images = [
+    "/partners/casaTropical.jpg",
+    "/partners/concid.png",
+    "/partners/oakberry.png",
+    "/partners/pegasus.jpg",
+    "/partners/superBowls.png",
+    "/partners/tulum.png",
+];
+
 type ComponentType = React.FC<BusinessesPageViewProps>;
+
 export const BusinessesPageView: ComponentType = () => {
     const { commonLocale, templatesLocale } = useLocale();
+
     return (
-        <section id="/businesses" className="bg-gray-100">
+        <S.Container id="/businesses">
             <Header
                 title={commonLocale.get("opportunities")}
                 subtitle={templatesLocale.get("partnerUp")}
             />
-        </section>
+
+            <InfiniteCarousel images={images} speed={20} />
+        </S.Container>
     );
 };
